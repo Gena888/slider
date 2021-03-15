e308929b96b73cbbea9f65c0ca511cac3c46d431
Gena888 <TheGena888@gmail.com>
2021-03-15 04:07:15 +0300

    feat: ready

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 let items = document.querySelectorAll('.carousel .item')
 let line = document.querySelector('.items-line')
-let leftBtn = document.querySelector('.control .left')
-let rightBtn = document.querySelector('.control .right')
+let leftBtn = document.querySelector('.control.left')
+let rightBtn = document.querySelector('.control.right')
 let offset = 0
 
-leftBtn.addEventListener('click', function() {
-    // let left = line.style('letf')
-    // console.log(left)
-    offset += 288
-    line.style.left = `${offset}px`
-    
+leftBtn.addEventListener('click', function () {
+    if (offset === 0) {
+        return
+    } else {
+        offset += 288
+        line.style.left = `${offset}px`
+    }
+
 })
 
-rightBtn.addEventListener('click', function() {
-    offset -= 288
-    line.style.left = offset + 'px'
+rightBtn.addEventListener('click', function () {
+    if (offset === -288*4) {
+        return
+    } else {
+        offset -= 288
+        line.style.left = offset + 'px'
+    }
 })
