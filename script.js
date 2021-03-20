9e0e0a08bcff8a44808bfc6100e371aeec8e5245
Gena888 <TheGena888@gmail.com>
2021-03-20 12:53:30 +0300

    feat: add rage input

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let line = document.querySelector('.items-line')
 let leftBtn = document.querySelector('.control.left')
 let rightBtn = document.querySelector('.control.right')
 let offset = 0
+let rage = document.querySelector('.rage-input')
 
 leftBtn.addEventListener('click', function () {
     if (offset === 0) {
@@ -10,15 +11,52 @@ leftBtn.addEventListener('click', function () {
     } else {
         offset += 288
         line.style.left = `${offset}px`
+        rage.value--
     }
 
 })
 
 rightBtn.addEventListener('click', function () {
-    if (offset === -288*4) {
+    if (offset === -288 * 4) {
         return
     } else {
         offset -= 288
         line.style.left = offset + 'px'
+        rage.value++
     }
+})
+
+let currentValue
+
+rage.addEventListener('pointerdown', (e) => {
+    currentValue = e.target.value
+
+    // console.log(`currentValue = ${currentValue}`)
+    console.log('mousedown')
+
+
+
+})
+
+rage.addEventListener('pointerup', (e) => {
+    // debugger
+    let addOffset = (value) => {
+        if (value > currentValue) {
+            // debugger
+            for (i = currentValue; i < value; i++) {
+                offset = offset - 288
+            }
+        } else if (value < currentValue) {
+            for (i = currentValue; i > value; i--) {
+                offset = offset + 288
+            }
+        } else {
+            return
+        }
+    }
+    addOffset(e.target.value)
+
+    line.style.left = `${offset}px`
+
+
 })
